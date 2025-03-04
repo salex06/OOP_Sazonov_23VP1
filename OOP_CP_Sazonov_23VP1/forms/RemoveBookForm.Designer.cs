f010d695a547312c490864d54c4f0fdbaadd97d3
Alexey Sazonov <salex.9206@mail.ru>
2025-03-04 22:14:20 +0300

    fix: изменено названиие TextBox
    В форме удаления информационное текстовое поле
    переименовано с textBox1 на removeBookInfoText

--- OOP_CP_Sazonov_23VP1/forms/RemoveBookForm.Designer.cs
+++ OOP_CP_Sazonov_23VP1/forms/RemoveBookForm.Designer.cs
@@ -31,7 +31,7 @@
             deleteFormLabel = new Label();
             deleteBookIdNumericUpDown = new NumericUpDown();
             deleteBookButton = new Button();
-            textBox1 = new TextBox();
+            removeBookInfoTextBox = new TextBox();
             deleteExtraPanel = new Panel();
             deleteFormPanel = new Panel();
             ((System.ComponentModel.ISupportInitialize)deleteBookIdNumericUpDown).BeginInit();
@@ -53,7 +53,7 @@
             // 
             deleteBookIdNumericUpDown.Anchor = AnchorStyles.None;
             deleteBookIdNumericUpDown.AutoSize = true;
-            deleteBookIdNumericUpDown.Location = new Point(321, 23);
+            deleteBookIdNumericUpDown.Location = new Point(322, 23);
             deleteBookIdNumericUpDown.Name = "deleteBookIdNumericUpDown";
             deleteBookIdNumericUpDown.Size = new Size(180, 31);
             deleteBookIdNumericUpDown.TabIndex = 1;
@@ -64,30 +64,30 @@
             deleteBookButton.BackColor = Color.Coral;
             deleteBookButton.Dock = DockStyle.Right;
             deleteBookButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            deleteBookButton.Location = new Point(372, 0);
+            deleteBookButton.Location = new Point(374, 0);
             deleteBookButton.Name = "deleteBookButton";
             deleteBookButton.Size = new Size(191, 60);
             deleteBookButton.TabIndex = 0;
             deleteBookButton.Text = "Удалить книгу";
             deleteBookButton.UseVisualStyleBackColor = false;
             // 
-            // textBox1
+            // removeBookInfoTextBox
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            textBox1.Location = new Point(-85, 17);
-            textBox1.Name = "textBox1";
-            textBox1.ReadOnly = true;
-            textBox1.Size = new Size(378, 31);
-            textBox1.TabIndex = 1;
+            removeBookInfoTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            removeBookInfoTextBox.Location = new Point(16, 17);
+            removeBookInfoTextBox.Name = "removeBookInfoTextBox";
+            removeBookInfoTextBox.ReadOnly = true;
+            removeBookInfoTextBox.Size = new Size(317, 31);
+            removeBookInfoTextBox.TabIndex = 1;
             // 
             // deleteExtraPanel
             // 
-            deleteExtraPanel.Controls.Add(textBox1);
+            deleteExtraPanel.Controls.Add(removeBookInfoTextBox);
             deleteExtraPanel.Controls.Add(deleteBookButton);
             deleteExtraPanel.Dock = DockStyle.Bottom;
-            deleteExtraPanel.Location = new Point(0, 107);
+            deleteExtraPanel.Location = new Point(0, 112);
             deleteExtraPanel.Name = "deleteExtraPanel";
-            deleteExtraPanel.Size = new Size(563, 60);
+            deleteExtraPanel.Size = new Size(565, 60);
             deleteExtraPanel.TabIndex = 1;
             // 
             // deleteFormPanel
@@ -97,7 +97,7 @@
             deleteFormPanel.Dock = DockStyle.Top;
             deleteFormPanel.Location = new Point(0, 0);
             deleteFormPanel.Name = "deleteFormPanel";
-            deleteFormPanel.Size = new Size(563, 75);
+            deleteFormPanel.Size = new Size(565, 75);
             deleteFormPanel.TabIndex = 0;
             // 
             // RemoveBookForm
@@ -105,7 +105,7 @@
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.AntiqueWhite;
-            ClientSize = new Size(563, 167);
+            ClientSize = new Size(565, 172);
             Controls.Add(deleteExtraPanel);
             Controls.Add(deleteFormPanel);
             Name = "RemoveBookForm";
@@ -122,7 +122,7 @@
         private NumericUpDown deleteBookIdNumericUpDown;
         private Label deleteFormLabel;
         private Button deleteBookButton;
-        private TextBox textBox1;
+        private TextBox removeBookInfoTextBox;
         private Panel deleteExtraPanel;
         private Panel deleteFormPanel;
     }
