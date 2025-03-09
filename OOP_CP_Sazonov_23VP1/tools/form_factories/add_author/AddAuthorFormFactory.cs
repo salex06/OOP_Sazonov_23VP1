a46f324d2ff5d494bef4eb029a279291dfed0af4
Alexey Sazonov <salex.9206@mail.ru>
2025-03-09 09:01:33 +0300

    feat: добавлены фабрики для форм
    Теперь для получения форм используются
    фабрики, которые обращаются к зарегистрированным
    компонентам системы через ServiceProvider

---
+++ OOP_CP_Sazonov_23VP1/tools/form_factories/add_author/AddAuthorFormFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OOP_CP_Sazonov_23VP1.forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CP_Sazonov_23VP1.tools.form_factories.add_author
+{
+    public class AddAuthorFormFactory : IAddAuthorFormFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public AddAuthorFormFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public NewAuthorForm Create()
+        {
+            return _serviceProvider.GetRequiredService<NewAuthorForm>();
+        }
+    }
+}
---
+++ OOP_CP_Sazonov_23VP1/tools/form_factories/add_book/AddBookFormFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OOP_CP_Sazonov_23VP1.forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CP_Sazonov_23VP1.tools.form_factories.add_book
+{
+    class AddBookFormFactory : IAddBookFormFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public AddBookFormFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public AddBookForm Create()
+        {
+            return _serviceProvider.GetRequiredService<AddBookForm>();
+        }
+    }
+}
